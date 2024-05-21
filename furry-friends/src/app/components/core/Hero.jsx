88141682a52a9155b3b79073724228eabb369a0e
Mahama Hamida <mahamahamida@gmail.com>
2024-05-21 17:54:13 +0000

    feat: Add sign up and sign in routes

--- furry-friends/src/app/components/core/Hero.jsx
+++ furry-friends/src/app/components/core/Hero.jsx
@@ -7,14 +7,18 @@ export default function Hero() {
       <header
         className="px-4 hero-pattern w-full hero-bg text-dark"
         style={{
+          backgroundColor: "#FFCDD2",
           backgroundImage:
             "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716221277/paws-bg_nkyk44.png')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
         }}
       >
-        <div className="md:flex flex-row justify-between ms-12">
+        <div className="md:flex flex-row justify-between md:ms-12">
           <div className="mb-20 ">
             <p
-              className={`text-[40px] pt-24 font-bold leading-none md:text-6xl ${merriweather.className}`}
+              className={`text-[40px] pt-14 md:pt-24 font-bold leading-none md:text-6xl ${merriweather.className}`}
             >
               WELCOME TO
             </p>
@@ -27,28 +31,66 @@ export default function Hero() {
             </p>
             <button
               type="button"
-              style={{backgroundColor: 'var(--accent-100)'}}
+              style={{ backgroundColor: "var(--accent-100)" }}
               className="text-white flex flex-row items-center gap-4  hover: font-medium rounded-full text-sm px-8 py-4 border-2 border-black shadow-[4px_4px_0px_0px_#000000] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Fetch your new friend <ArrowRight2 size="16" color="#ffffff" variant="Outline" />
+              Fetch your new friend{" "}
+              <ArrowRight2 size="16" color="#ffffff" variant="Outline" />
             </button>
           </div>
-          <img className="md:h-[600px]" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716221190/person-creating-online-content-with-their-pets-removebg_1_pw0x9h.png"></img>
+          <img
+            className="md:h-[600px]"
+            src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716221190/person-creating-online-content-with-their-pets-removebg_1_pw0x9h.png"
+          ></img>
         </div>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non reprehenderit repudiandae ullam delectus error, ducimus neque quos ut exercitationem quaerat a expedita soluta tenetur perferendis est iusto commodi iste?
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat non
+          reprehenderit repudiandae ullam delectus error, ducimus neque quos ut
+          exercitationem quaerat a expedita soluta tenetur perferendis est iusto
+          commodi iste? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Fugiat non reprehenderit repudiandae ullam delectus error, ducimus
+          neque quos ut exercitationem quaerat a expedita soluta tenetur
+          perferendis est iusto commodi iste? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Fugiat non reprehenderit repudiandae
+          ullam delectus error, ducimus neque quos ut exercitationem quaerat a
+          expedita soluta tenetur perferendis est iusto commodi iste? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Fugiat non
+          reprehenderit repudiandae ullam delectus error, ducimus neque quos ut
+          exercitationem quaerat a expedita soluta tenetur perferendis est iusto
+          commodi iste? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Fugiat non reprehenderit repudiandae ullam delectus error, ducimus
+          neque quos ut exercitationem quaerat a expedita soluta tenetur
+          perferendis est iusto commodi iste? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Fugiat non reprehenderit repudiandae
+          ullam delectus error, ducimus neque quos ut exercitationem quaerat a
+          expedita soluta tenetur perferendis est iusto commodi iste? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Fugiat non
+          reprehenderit repudiandae ullam delectus error, ducimus neque quos ut
+          exercitationem quaerat a expedita soluta tenetur perferendis est iusto
+          commodi iste? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Fugiat non reprehenderit repudiandae ullam delectus error, ducimus
+          neque quos ut exercitationem quaerat a expedita soluta tenetur
+          perferendis est iusto commodi iste? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Fugiat non reprehenderit repudiandae
+          ullam delectus error, ducimus neque quos ut exercitationem quaerat a
+          expedita soluta tenetur perferendis est iusto commodi iste? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Fugiat non
+          reprehenderit repudiandae ullam delectus error, ducimus neque quos ut
+          exercitationem quaerat a expedita soluta tenetur perferendis est iusto
+          commodi iste? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Fugiat non reprehenderit repudiandae ullam delectus error, ducimus
+          neque quos ut exercitationem quaerat a expedita soluta tenetur
+          perferendis est iusto commodi iste? Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Fugiat non reprehenderit repudiandae
+          ullam delectus error, ducimus neque quos ut exercitationem quaerat a
+          expedita soluta tenetur perferendis est iusto commodi iste? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Fugiat non
+          reprehenderit repudiandae ullam delectus error, ducimus neque quos ut
+          exercitationem quaerat a expedita soluta tenetur perferendis est iusto
+          commodi iste? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Fugiat non reprehenderit repudiandae ullam delectus error, ducimus
+          neque quos ut exercitationem quaerat a expedita soluta tenetur
+          perferendis est iusto commodi iste?
         </p>
       </header>
     </>
