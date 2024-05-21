84f7dc6b29083ebe13d2d69a9d68203d548b4391
Mahama Hamida <mahamahamida@gmail.com>
2024-05-21 01:32:52 +0000

    refactor: Remove unused code and update layout.js

--- furry-friends/src/app/layout.js
+++ furry-friends/src/app/layout.js
@@ -1,8 +1,10 @@
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
+import { inter } from "./fonts";
 import "./globals.css";
 import NavBar from "./components/core/NavBar";
+import Hero from "./components/core/Hero";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -14,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
+        <Hero />
         {children}
       </body>
     </html>
