bca6c76ee36160c6fb6c26ba522c038fa7e75435
Mahama Hamida <mahamahamida@gmail.com>
2024-05-21 17:54:22 +0000

    refactor: Update NavBar component and add Link for logo and signup button

--- furry-friends/src/app/components/core/NavBar.jsx
+++ furry-friends/src/app/components/core/NavBar.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { HambergerMenu } from "iconsax-react";
 import { useState } from "react";
+import Link from "next/link";
 
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,26 +10,28 @@ export default function NavBar() {
     <>
       <nav className="bg-gray-900 backdrop-filter backdrop-blur-md bg-opacity-50 border-b-[1px] border-gray-500 dark:bg-gray-900 sticky z-20 top-0 start-0 w-full">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
+          <Link
+            href="/landingpage"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img src="/logo.svg"></img>
-          </a>
+          </Link>
           <div className="flex gap-2 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
-            //   style={{backgroundColor: 'var(--accent-100)'}}
-              className=" hidden md:block text-accent bg-white lg:px-8 py-4 font-medium rounded-full text-sm px-8 text-center "
+              //   style={{backgroundColor: 'var(--accent-100)'}}
+              className=" hidden md:block text-accent bg-white lg:px-8 py-4 font-medium rounded-full text-sm px-8 text-center hover:bg-red-100 "
             >
               Log in
             </button>
             <button
               type="button"
-              style={{backgroundColor: 'var(--accent-100)'}}
-              className="text-white lg:px-8 py-4 font-medium rounded-full text-sm px-8 text-center"
+              style={{ backgroundColor: "var(--accent-100)" }}
+              className="text-white lg:px-8 py-4 font-medium rounded-full text-sm px-8 text-center hover:bg-red-500"
             >
-              Sign up
+              <Link href="/signup">
+                Sign up
+              </Link>
             </button>
             <button
               data-collapse-toggle="navbar-cta"
@@ -39,7 +42,11 @@ export default function NavBar() {
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
               <span className="sr-only">Open main menu</span>
-              <HambergerMenu size='32' aria-hidden="true" xmlns="http://www.w3.org/2000/svg" />
+              <HambergerMenu
+                size="32"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+              />
               {/* <svg
                 className="w-5 h-5"
                 aria-hidden="true"
@@ -57,8 +64,11 @@ export default function NavBar() {
               </svg> */}
             </button>
           </div>
-          <div className={`items-center justify-between ${isMenuOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} 
-          id="navbar-cta"
+          <div
+            className={`items-center justify-between ${
+              isMenuOpen ? "block" : "hidden"
+            } w-full md:flex md:w-auto md:order-1`}
+            id="navbar-cta"
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md: dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
@@ -66,7 +76,7 @@ export default function NavBar() {
                   href="#"
                   className="block py-2 px-3 md:p-0 text-white rounded md:bg-transparent md:text-secondary md:dark:text-secondary"
                   aria-current="page"
-                  style={{color:'var(--secondary)'}}
+                  style={{ color: "var(--secondary)" }}
                 >
                   Home
                 </a>
@@ -76,7 +86,7 @@ export default function NavBar() {
                   href="#"
                   className="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
-                 Why adopt?
+                  Why adopt?
                 </a>
               </li>
               <li>
