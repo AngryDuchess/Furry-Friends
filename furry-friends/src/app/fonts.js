924388af241c108202215a0ebf62fe4b2777d291
Mahama Hamida <mahamahamida@gmail.com>
2024-05-21 01:32:28 +0000

    feat: Add fonts.js file for custom fonts in Furry Friends website

---
+++ furry-friends/src/app/fonts.js
@@ -0,0 +1,12 @@
+import { Inter, Merriweather} from 'next/font/google';
+
+export const inter = Inter({ 
+    subsets: ["latin"] ,
+    display: 'swap',
+});
+
+export const merriweather = Merriweather({
+    subsets: ["latin"] ,
+    display: 'swap',
+    weight: '900'
+})
