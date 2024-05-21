bcb0d17d749b3edfb8a9ef5872171b0d7a9491e0
Mahama Hamida <mahamahamida@gmail.com>
2024-05-21 17:54:05 +0000

    create route for sign up

---
+++ furry-friends/src/app/(auth)/signup/page.jsx
@@ -0,0 +1,127 @@
+import { merriweather } from "@/app/fonts";
+
+export default function Page() {
+  return (
+    <>
+      <div className="flex flex-col-reverse md:flex-row justify-between h-screen">
+        <div className="flex-grow mt-5 mb-12 mx-4 overflow-y-auto">
+          <p
+            className={`text-dark ${merriweather.className} text-4xl mb-2 md:mt-14 `}
+          >
+            Sign Up!
+          </p>
+          <p className="text-dark text-sm font-semibold mb-5">
+            You are one step closer to finding your new best friend 💞
+          </p>
+          <form className="max-w-sm">
+            <div class="mb-5">
+              <label
+                htmlFor="text"
+                className="block mb-2 text-sm font-semibold text-dark dark:text-white"
+              >
+                First name
+              </label>
+              <input
+                type="text"
+                id="firstname"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-secondary focus:border-secondarydeep block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondary dark:focus:border-secondarydeep"
+                placeholder="Your first name eg: Teddy"
+                required
+              />
+            </div>
+            <div class="mb-5">
+              <label
+                htmlFor="text"
+                className="block mb-2 text-sm font-semibold text-dark dark:text-white"
+              >
+                Last name
+              </label>
+              <input
+                type="text"
+                id="lastname"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-secondary focus:border-secondarydeep block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondary dark:focus:border-secondarydeep"
+                placeholder="Your last name eg: Mahama"
+                required
+              />
+            </div>
+            <div class="mb-5">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-semibold text-dark dark:text-white"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-secondary focus:border-secondarydeep block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondary dark:focus:border-secondarydeep"
+                placeholder="teddy@furryfriends.com"
+                required
+              />
+            </div>
+            <div class="mb-5">
+              <label
+                htmlFor="number "
+                className="block mb-2 text-sm font-semibold text-dark dark:text-white"
+              >
+                Phone
+              </label>
+              <input
+                type="number"
+                id="phone"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-secondary focus:border-secondarydeep block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondary dark:focus:border-secondarydeep"
+                placeholder="Your phone number"
+                required
+              />
+            </div>
+            <div class="mb-5">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm font-semibold text-dark dark:text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-secondary focus:border-secondarydeep block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondary dark:focus:border-secondarydeep"
+                placeholder="Your new password"
+                required
+              />
+            </div>
+            <div class="flex items-start mb-5">
+              <div class="flex items-center h-5">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 border border-gray-300 rounded checked:bg-accent bg-gray-50 focus:ring-3 focus:ring-accentpaler dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <label
+                for="remember"
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Show password
+              </label>
+            </div>
+            <button
+              type="submit"
+              style={{ backgroundColor: "var(--accent-100)" }}
+              className="text-white lg:px-8 py-4 font-medium rounded-full text-sm px-8 text-center hover:bg-red-500 w-full"
+            >
+                Let's go!
+            </button>
+          </form>
+        </div>
+        <div className="bg-palepink flex md:flex-grow justify-center items-center overflow-hidden">
+          <img
+            className="h-[200px]"
+            src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716303452/421ba436690245.57260cab17a3a_zjwjmt.gif"
+          ></img>
+        </div>
+      </div>
+    </>
+  );
+}
