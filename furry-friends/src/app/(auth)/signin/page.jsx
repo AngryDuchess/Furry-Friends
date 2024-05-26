77d14a368a0b04ef2d1a2ab56ae3ffdbe970abd8
Mahama Hamida <mahamahamida@gmail.com>
2024-05-26 22:24:54 +0000

    refactor: Include show password feature

--- furry-friends/src/app/(auth)/signin/page.jsx
+++ furry-friends/src/app/(auth)/signin/page.jsx
@@ -10,15 +10,15 @@ import isAuthenticated from "@/lib/isAuthenticated";
 
 export default function Page() {
   const router = useRouter();
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [error, setError] = useState(false);
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleEmailChange = (e) => setEmail(e.target.value);
 
-  const handlePaswordChange = (e) => setPassword(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
 
   const handleSignIn = (e) => {
     e.preventDefault();
@@ -37,8 +37,6 @@ export default function Page() {
     if (isAuthenticated()) router.push("/home");
   };
 
-  console.log(error);
-
   return (
     <>
       <div className="flex flex-col-reverse md:flex-row justify-between h-screen">
@@ -47,7 +45,7 @@ export default function Page() {
             href="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img src="/logo.svg"></img>
+            <img src="/logo.svg" alt="Logo" />
           </Link>
           <p
             className={`text-dark ${merriweather.className} text-4xl mb-2 md:mt-14 `}
@@ -57,10 +55,7 @@ export default function Page() {
           <p className="text-dark text-sm font-semibold mb-5">
             Welcome back, Hooman! 🥹
           </p>
-          <form
-            className="max-w-sm flex flex-col gap-5"
-            onSubmit={handleSignIn}
-          >
+          <form className="max-w-sm flex flex-col gap-5" onSubmit={handleSignIn}>
             {error && (
               <Alert
                 color={"info"}
@@ -88,7 +83,6 @@ export default function Page() {
                 placeholder="teddy@furryfriends.com"
                 required
               />
-              {/* <p>Oops! your email is required.</p> */}
             </div>
             <div className="gap-2">
               <div className="mb-2">
@@ -99,10 +93,10 @@ export default function Page() {
                   Password
                 </label>
                 <input
-                  type="password"
+                  type={showPassword ? "text" : "password"}
                   id="password"
                   value={password}
-                  onChange={handlePaswordChange}
+                  onChange={handlePasswordChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-full focus:ring-secondary focus:border-secondarydeep block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-secondary dark:focus:border-secondarydeep"
                   placeholder="Your new password"
                   required
@@ -111,26 +105,25 @@ export default function Page() {
               <div className="flex items-start mb-5">
                 <div className="flex items-center h-5">
                   <input
-                    id="remember"
+                    id="showpassword"
                     type="checkbox"
-                    value=""
+                    checked={showPassword}
+                    onChange={(e) => setShowPassword(e.target.checked)}
                     className="w-4 h-4 border border-gray-300 rounded checked:bg-accent bg-gray-50 focus:ring-3 focus:ring-accentpaler dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
                   />
                 </div>
                 <label
-                  htmlFor="remember"
+                  htmlFor="showpassword"
                   className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
                   Show password
                 </label>
               </div>
             </div>
-            {/* <p className="text-light text-sm">By signing up, you agree to our <Link className="text-bluebase hover:underline" href="/signin">terms and conditions</Link></p> */}
             <div className="text-center">
               <button
                 type="submit"
                 disabled={loading}
-                // onClick={handleSignIn}
                 className="flex justify-center text-white mb-3 lg:px-8 py-4 mt-12 font-medium rounded-full text-sm px-8 text-center bg-accent hover:bg-accentdeep w-full"
               >
                 {loading ? <Spinner /> : "Let's go!"}
@@ -148,6 +141,7 @@ export default function Page() {
           <img
             className="h-[200px]"
             src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716303452/421ba436690245.57260cab17a3a_zjwjmt.gif"
+            alt="Cat gif"
           ></img>
         </div>
       </div>
