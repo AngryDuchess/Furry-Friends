ce8cc5773b5b2487c7d1db4cbae9475e1e9111cc
Mahama Hamida <mahamahamida@gmail.com>
2024-05-27 14:59:25 +0000

    feat: Add NavBar component to homepage

--- furry-friends/src/app/home/page.jsx
+++ furry-friends/src/app/home/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
 import HowItWorks from "../components/HowItWorks";
+import NavBar from "../components/core/NavBar";
 
 export default function Page() {
   return (
     <>
+      <NavBar />
       <Hero />
       <HowItWorks />
     </>
