f54bd9f18b696f697afe30e2312abaf15fe151f0
Mahama Hamida <mahamahamida@gmail.com>
2024-05-29 22:48:51 +0000

    refactor: Remove commented code and unused imports in Pet Details Page component

--- furry-friends/src/app/pets/details/page.jsx
+++ furry-friends/src/app/pets/details/page.jsx
@@ -5,7 +5,17 @@ import AdoptionFormModal from "@/app/components/modals/AdoptionForm";
 import { useState } from "react";
 
 export default function Page() {
-  const [openModal, setOpenModal]= useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  // const [openModal, setOpenModal] = useState(false);
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setLoading(true);
+  //   setTimeout(() => {
+  //     setOpenModal(false)
+  //   }, 2000)
+  // };
+
   return (
     <>
       <section className="mx-4 lg:mx-16 my-16 flex flex-col lg:flex-row gap-4 lg:gap-8 text-dark font-semibold">
@@ -16,9 +26,7 @@ export default function Page() {
           />
         </div>
         <div className="flex flex-col gap-6 lg:max-w-sm">
-          <div
-            className={`${merriweather.className} text-2xl text-dark flex justify-between`}
-          >
+          <div className={`${merriweather.className} text-2xl text-dark flex justify-between`}>
             <p>Beagle puppy</p>
             <p className="">$ 250.00</p>
           </div>
@@ -44,28 +52,25 @@ export default function Page() {
               <p>2 months old</p>
             </div>
           </div>
-        <div className="flex flex-col items-start gap-4 rounded-xl p-4 bg-secondarypale border border-secondary">
-          <p>Hi there, I'm a beagle puppy!</p>
-          <p className="font-normal text-sm">
-            I'm a playful Beagle puppy with a nose for adventure and a heart
-            full of love. I love sniffing around, playing fetch, and cuddling up
-            for naps. My floppy ears and wagging tail are ready to bring endless
-            joy to your home. Are you ready for a lifetime of fun and snuggles?
-          </p>
-          <button
-            type="button"
-            className="text-white bg-bluebase flex flex-row items-center justify-between gap-4 font-medium rounded-full text-sm px-8 py-4 border-2 border-black shadow-[4px_4px_0px_0px_#000000] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={() => {setOpenModal(true)}}
-          >
-            {openModal && 
-                (<AdoptionFormModal isOpen={openModal} onClose={() => setOpenModal(false)}/>)
-                }
-            I'm the one!{" "}
-            <ArrowRight2 size="16" color="#ffffff" variant="Outline" />
-          </button>
-        </div>
+          <div className="flex flex-col items-start gap-4 rounded-xl p-4 bg-secondarypale border border-secondary">
+            <p>Hi there, I'm a beagle puppy!</p>
+            <p className="font-normal text-sm">
+              I'm a playful Beagle puppy with a nose for adventure and a heart full of love. I love sniffing around, playing fetch, and cuddling up for naps. My floppy ears and wagging tail are ready to bring endless joy to your home. Are you ready for a lifetime of fun and snuggles?
+            </p>
+            <button
+              type="button"
+              className="text-white bg-bluebase flex flex-row items-center justify-between gap-4 font-medium rounded-full text-sm px-8 py-4 border-2 border-black shadow-[4px_4px_0px_0px_#000000] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => setOpenModal(true)}
+            >
+              I'm the one!{" "}
+              <ArrowRight2 size="16" color="#ffffff" variant="Outline" />
+            </button>
+          </div>
         </div>
       </section>
+      {openModal && (
+        <AdoptionFormModal isOpen={openModal} onClose={() => setOpenModal(false)} />
+      )}
     </>
   );
 }
