372b2a602942122a6f35186efcc99dc1a47fdd3c
Mahama Hamida <mahamahamida@gmail.com>
2024-05-30 03:39:12 +0000

    feat: Add DonationCompleteModal component

---
+++ furry-friends/src/app/components/modals/DonationComplete.jsx
@@ -0,0 +1,14 @@
+import { Modal } from "flowbite-react";
+
+export default function DonationCompleteModal() {
+    return (
+        <>
+        <div className="max-w-sm mx-auto">
+
+            <Modal>
+
+            </Modal>
+        </div>
+        </>
+    )
+}
