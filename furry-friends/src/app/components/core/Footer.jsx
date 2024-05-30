f986b71c18ed5727c6bad4e8e898518d7eed72d6
Mahama Hamida <mahamahamida@gmail.com>
2024-05-30 19:03:55 +0000

    refactor: Update Footer component styling for responsiveness

--- furry-friends/src/app/components/core/Footer.jsx
+++ furry-friends/src/app/components/core/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     const current_year = new Date();
   return (
-    <footer className="text-dark text-sm p-5 border-t border-gray-400 flex flex-col justify-between items-end lg:flex-row lg:px-16 gap-8">
+    <footer className="text-dark text-sm p-4 border-t border-gray-400 flex flex-col lg:justify-between lg:flex-row lg:px-16 gap-8">
       <div className=" flex flex-col gap-2 items-start">
         <img src="/logo.svg" />
         <p className="font-medium">
@@ -14,7 +14,7 @@ export default function Footer() {
         <p class>Built with love ❤️. © {current_year.getFullYear()} Hamida Mahama</p>
       </div>
       <div>
-        <ul className="flex flex-col lg:flex-row gap-8">
+        <ul className="flex flex-col gap-3 lg:flex-row lg:gap-8">
             <li><a className="underline lg:no-underline lg:hover:underline" href="https://www.linkedin.com/in/hamidamahama/">LINKEDIN</a></li>
             <li><a className="underline lg:no-underline lg:hover:underline" href="https://www.linkedin.com/in/hamidamahama/">GITHUB</a></li>
             <li><a className="underline lg:no-underline lg:hover:underline" href="https://www.linkedin.com/in/hamidamahama/">TWITTER</a></li>
