444d2e78a603a56f70ca4123fb902727345cc0de
Mahama Hamida <mahamahamida@gmail.com>
2024-06-01 01:45:34 +0000

    refactor: Add lazy loading for pet images on the PetCard component

--- furry-friends/src/app/components/PetCard.jsx
+++ furry-friends/src/app/components/PetCard.jsx
@@ -3,24 +3,27 @@ import { Card, Dropdown } from "flowbite-react";
 import Image from "next/image";
 import NavBar from "./core/NavBar";
 import { SearchNormal1 } from "iconsax-react";
+import Link from "next/link";
 
-export default function PetCard() {
+export default function PetCard({image, name, url, id}) {
   return (
     <>
       <Card
         className="max-w-[316px] border-none gap-2"
         renderImage={() => (
+          <Link href={`/pets/details/${id}`}>
           <Image
-          className="rounded-xl"
+          className="rounded-xl max-w-full min-h-[300px] max-h-[300px] object-cover"
             width={500}
             height={500}
-            src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1715422407/samples/animals/three-dogs.jpg"
-            alt="image 1"
+            src={image}
+            alt={name}
           />
+          </Link>
         )}
       >
         <p className="font-bold text-dark hover:text-accent  dark:text-gray-400">
-          Beagle puppy
+          {name}
         </p>
       </Card>
     </>
