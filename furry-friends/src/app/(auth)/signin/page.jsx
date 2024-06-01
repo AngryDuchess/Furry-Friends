7d89d6cfc9fa31c0d66d9914d432a852153dcce9
Mahama Hamida <mahamahamida@gmail.com>
2024-06-01 23:57:39 +0000

    refactor: Remove unnecessary code for user authentication in signin page

--- furry-friends/src/app/(auth)/signin/page.jsx
+++ furry-friends/src/app/(auth)/signin/page.jsx
@@ -30,7 +30,6 @@ export default function Page() {
     }
     const user = { email };
     setTimeout(() => {
-      // isAuthenticated(user);
       localStorage.setItem("user",JSON.stringify(user))
       console.log(user);
       setLoading(false);
