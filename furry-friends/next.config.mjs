4eaad2a090fa378bd4bd076c5eb62e9104f60323
Mahama Hamida <mahamahamida@gmail.com>
2024-06-02 01:51:08 +0000

    feat: Add support for fetching dog images from thedogapi.com

--- furry-friends/next.config.mjs
+++ furry-friends/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['res.cloudinary.com', 'images.dog.ceo', 'cdn2.thecatapi.com'],
+    domains: ['res.cloudinary.com', 'images.dog.ceo', 'cdn2.thecatapi.com', 'cdn2.thedogapi.com'],
   },
 };
 
