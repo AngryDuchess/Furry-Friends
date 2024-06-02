17604b9cccf59be6e02d7b8f9d91913548c1d410
Mahama Hamida <mahamahamida@gmail.com>
2024-06-02 01:53:50 +0000

    feat: Update API endpoints to fetch limited number of cat and dog breeds

--- furry-friends/src/lib/api.js
+++ furry-friends/src/lib/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const fetchCats = async () => {
     try {
-        const res = await axios.get(`https://api.thecatapi.com/v1/breeds?limit=10`, {
+        const res = await axios.get(`https://api.thecatapi.com/v1/breeds?limit=3`, {
           headers: {
             'x-api-key': process.env.NEXT_PUBLIC_API_KEY
           },
@@ -13,7 +13,33 @@ const fetchCats = async () => {
     }
 }
 
-const getImage = async (id) => {
+const fetchDogs = async () => {
+  try {
+      const res = await axios.get(`https://api.thedogapi.com/v1/breeds?limit=10`, {
+        headers: {
+          'x-api-key': process.env.NEXT_PUBLIC_API_KEY
+        },
+      })
+      return res.data
+  } catch(error) {
+      console.log(error)
+  }
+}
+
+const getDogImage = async (id) => {
+  try {
+    const res = await axios.get(`https://api.thedogapi.com/v1/images/${id}`, {
+      headers: {
+        'x-api-key': process.env.NEXT_PUBLIC_API_KEY
+      }
+    })
+    return res.data?.url
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+const getCatImage = async (id) => {
   try {
     const res = await axios.get(`https://api.thecatapi.com/v1/images/${id}`, {
       headers: {
@@ -26,7 +52,7 @@ const getImage = async (id) => {
   }
 }
 
-const getPetDetails = async (id) => {
+const getCatDetails = async (id) => {
   try {
     const res = await axios.get(`https://api.thecatapi.com/v1/images/${id}`, {
       headers: {
@@ -35,12 +61,25 @@ const getPetDetails = async (id) => {
     })
     return res.data
   } catch(error) {
+    try {
+      const res = await axios.get(`https://api.thedogapi.com/v1/images/${id}`, {
+        headers: {
+          'x-api-key': process.env.NEXT_PUBLIC_API_KEY
+        }
+      })
+      return res.data
+    } catch(error) {
+      console.log(error)
+    }
     console.log(error)
   }
 }
 
+
 export {
     fetchCats,
-    getImage,
-    getPetDetails
+    getCatImage,
+    getDogImage,
+    getCatDetails,
+    fetchDogs
 }
