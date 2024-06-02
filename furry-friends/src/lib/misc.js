959a45d3bc902eb01dd43afde850f85f27e0f102
Mahama Hamida <mahamahamida@gmail.com>
2024-06-02 23:03:13 +0000

    refactor: Remove unnecessary code for shuffling array in misc.js

--- furry-friends/src/lib/misc.js
+++ furry-friends/src/lib/misc.js
@@ -1,6 +1,4 @@
 export const shuffleArray = (array) => {
-    /*  implementing Fisher-Yates algorithm rather than
-      using .sort() method */
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       const temp = array[i];
