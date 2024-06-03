75799c38ee58b073b41a974bc0edefea4ca2e49a
Mahama Hamida <mahamahamida@gmail.com>
2024-06-03 17:36:01 +0000

    feat: Implement lazy loading for images in WhyAdopt component

--- furry-friends/src/app/components/WhyAdopt.jsx
+++ furry-friends/src/app/components/WhyAdopt.jsx
@@ -2,13 +2,14 @@
 import { merriweather } from "@/app/fonts";
 import { ArrowRight2 } from "iconsax-react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function WhyAdopt() {
   const router = useRouter();
   return (
     <>
       <section
-        className="bg-white text-dark py-12 px-4 flex flex-col gap-14 md:px-16 md:flex-row"
+        className="bg-white text-dark py-12 px-4 flex flex-col gap-14 lg:px-16 lg:flex-row"
         style={{
           backgroundImage:
             "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716221277/paws-bg_nkyk44.png')",
@@ -30,7 +31,7 @@ export default function WhyAdopt() {
           </p>
           <button
               type="button"
-              className="text-white bg-accent flex flex-row items-center justify-between gap-4 hover: font-medium rounded-full text-sm px-8 py-4 border-2 border-black shadow-[4px_4px_0px_0px_#000000] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-accent flex flex-row items-center justify-between gap-4 hover:bg-accentdeep font-medium rounded-full text-sm px-8 py-4 border-2 border-black shadow-[4px_4px_0px_0px_#000000] text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             onClick={() => router.push('/why-adopt')}
             >
               Sniff out more{" "}
@@ -38,7 +39,7 @@ export default function WhyAdopt() {
             </button>
         </div>
         <div>
-          <img className="rounded-3xl" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716688770/smiley-woman-washing-dog-full-shot_2_3_yz7anj.png" />
+          <Image width={2000} height={2000} alt={'woman washing a dog'} className="rounded-3xl lg:w-[2000px] lg:h-auto" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716688770/smiley-woman-washing-dog-full-shot_2_3_yz7anj.png" />
         </div>
       </section>
     </>
