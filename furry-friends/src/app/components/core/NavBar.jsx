1d3c321b2a9c55a08d351b033adddd0c17023001
Mahama Hamida <mahamahamida@gmail.com>
2024-06-03 17:37:59 +0000

    feat: Implement lazy loading for images in signup and signin pages

--- furry-friends/src/app/components/core/NavBar.jsx
+++ furry-friends/src/app/components/core/NavBar.jsx
@@ -1,45 +1,47 @@
 "use client";
 import { HambergerMenu } from "iconsax-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
 import isAuthenticated from "@/lib/isAuthenticated";
 import { User } from "iconsax-react";
 import { Dropdown } from "flowbite-react";
+import Image from "next/image";
 
 export default function NavBar() {
   const router = useRouter();
-  const pathname = usePathname()
+  const pathname = usePathname();
+  const [user, setUser] = useState(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [LoggedIn, setLoggedIn] = useState(false);
   const [selectedTab, setSelectedTab] = useState(null);
-  const user = localStorage.getItem("user");
-  const userObj = JSON.parse(user);
-  const fullname = userObj.firstname + " " + userObj.lastname
-  console.log(userObj);
-  
+
   const handleLogout = () => {
-    localStorage.removeItem("user");
+    if (window && window !== "undefined") {
+      localStorage.removeItem("user");
+    }
     router.push("/signin");
   };
 
   const handleSelect = (tabitem) => {
     setSelectedTab(tabitem);
-  }
+  };
 
   const getLinkClassName = (path) => {
     return pathname === path ? 'text-secondary bg-accent lg:bg-none' : 'text-white';
   };
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setUser(JSON.parse(localStorage.getItem("user")));
+    }
+  }, []);
+
   return (
     <>
       <nav className="bg-gray-900 backdrop-filter backdrop-blur-md bg-opacity-50 border-b-[1px] border-gray-500 dark:bg-gray-900 sticky z-20 top-0 start-0 w-full">
         <div className="flex flex-wrap items-center justify-between mx-4 md:mx-16 p-4">
-          <Link
-            href="/"
-            className="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <img src="/logo.svg" alt="logo" />
+          <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <Image width={48} height={48} src="/logo.svg" alt="logo" />
           </Link>
           <div className="flex items-center gap-2 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             {isAuthenticated() ? (
@@ -50,9 +52,9 @@ export default function NavBar() {
                   label={<User size="24" color="#222222" variant="Bold" />}
                 >
                   <Dropdown.Header>
-                  <span className="block text-sm">{(fullname && fullname !== "undefined undefined") ? fullname : userObj.email }</span>
+                    <span className="block text-sm">{user?.firstname ? `${user.firstname} ${user.lastname}` : user?.email}</span>
                     <span className="block truncate text-sm font-medium">
-                     {userObj.email}
+                      {user?.email}
                     </span>
                   </Dropdown.Header>
                   <Dropdown.Item>Settings</Dropdown.Item>
@@ -65,7 +67,6 @@ export default function NavBar() {
                 <Link href="/signin">
                   <button
                     type="button"
-                    //   style={{backgroundColor: 'var(--accent-100)'}}
                     className=" hidden md:block text-accent bg-white lg:px-8 py-4 font-medium rounded-full text-sm px-8 text-center hover:bg-red-100 "
                   >
                     Log in
@@ -74,7 +75,6 @@ export default function NavBar() {
                 <Link href="/signup">
                   <button
                     type="button"
-                    // style={{ backgroundColor: "var(--accent-100)" }}
                     className="text-white lg:px-8 py-4 font-medium rounded-full text-sm px-8 text-center bg-accent hover:bg-accentdeep"
                   >
                     Sign up
@@ -85,34 +85,25 @@ export default function NavBar() {
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover: focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:focus:ring-gray-600"
               aria-controls="navbar-cta"
               aria-expanded="false"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
               <span className="sr-only">Open main menu</span>
-              <HambergerMenu
-                size="32"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-              />
+              <HambergerMenu size="32" aria-hidden="true" />
             </button>
           </div>
           <div
-            className={`items-center justify-between ${
-              isMenuOpen ? "block" : "hidden"
-            } w-full md:flex md:w-auto md:order-1`}
+            className={`items-center justify-between ${isMenuOpen ? "block" : "hidden"} w-full md:flex md:w-auto md:order-1`}
             id="navbar-cta"
           >
-            <ul className="flex flex-col text-sm font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md: dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col text-sm font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
                   href="/"
-                  // className={`${selectedTab ==='Home' ? 'text-secondary bg-accent lg:bg-none' : "lg:text-white"} block py-2 px-3 md:p-0 rounded md:bg-transparent md:text-secondary md:dark:text-secondary}`}
-                 className={`${getLinkClassName('/')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
-                 aria-current={router.pathname === '/' ? "page" : undefined}
-                  // aria-current={`${selectedTab ==='Home' ? "page" : undefined}`}
-                  // onClick={() => handleSelect("Home")}
+                  className={`${getLinkClassName('/')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
+                  aria-current={router.pathname === '/' ? "page" : undefined}
                 >
                   Home
                 </a>
@@ -120,11 +111,8 @@ export default function NavBar() {
               <li>
                 <a
                   href="/why-adopt"
-                 className={`${getLinkClassName('/why-adopt')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
-                 aria-current={router.pathname === '/why-adopt' ? "page" : undefined}
-                  // className={` ${selectedTab ==='Why adopt?' ? 'text-secondary bg-accent lg:bg-none' : "lg:text-white"} block py-2 px-3 md:p-0  rounded md:bg-transparent md:text-secondary md:dark:text-secondary}`}
-                  // aria-current={`${selectedTab ==='Why adopt?' ? "page" : undefined}`}
-                  // onClick={() => handleSelect("Why adopt?")}
+                  className={`${getLinkClassName('/why-adopt')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
+                  aria-current={router.pathname === '/why-adopt' ? "page" : undefined}
                 >
                   Why adopt?
                 </a>
@@ -132,11 +120,8 @@ export default function NavBar() {
               <li>
                 <a
                   href="/tips"
-                 className={`${getLinkClassName('/tips')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
-                 aria-current={router.pathname === '/tips' ? "page" : undefined}
-                  // className={` ${selectedTab ==='Paw-some tips' ? 'text-secondary bg-accent lg:bg-none' : "lg:text-white"} block py-2 px-3 md:p-0 rounded md:bg-transparent md:text-secondary md:dark:text-secondary}`}
-                  // aria-current={`${selectedTab ==='Paw-some tips' ? "page" : undefined}`}
-                  // onClick={() => handleSelect("Paw-some tips")}
+                  className={`${getLinkClassName('/tips')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
+                  aria-current={router.pathname === '/tips' ? "page" : undefined}
                 >
                   Paw-some tips
                 </a>
@@ -144,12 +129,8 @@ export default function NavBar() {
               <li>
                 <a
                   href="/pets"
-                 className={`${getLinkClassName('/pets')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
-                 aria-current={router.pathname === '/pets' ? "page" : undefined}
-                  // className={` ${selectedTab ==='Our pets' ? 'text-secondary bg-accent lg:bg-none' : "lg:text-white"} block py-2 px-3 md:p-0  rounded md:bg-transparent md:text-secondary md:dark:text-secondary}`}
-                  // aria-current={`${selectedTab ==="Our pets" ? "page" : undefined}`}
-                  // onClick={() => handleSelect("Our pets")}
-
+                  className={`${getLinkClassName('/pets')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
+                  aria-current={router.pathname === '/pets' ? "page" : undefined}
                 >
                   Our pets
                 </a>
@@ -157,12 +138,8 @@ export default function NavBar() {
               <li>
                 <a
                   href="/donate"
-                  // className={` ${selectedTab ==='Be a pet hero' ? 'text-secondary bg-accent lg:bg-none' : "lg:text-white"} block py-2 px-3 md:p-0 rounded md:bg-transparent md:text-secondary md:dark:text-secondary}`}
-                 className={`${getLinkClassName('/donate')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
-                 aria-current={router.pathname === '/donate' ? "page" : undefined}
-                // aria-current={`${selectedTab ==="Be a pet hero" ? "page" : undefined}`}
-                  
-                  // onClick={() => handleSelect("Be a pet hero")}
+                  className={`${getLinkClassName('/donate')} block py-2 px-3 md:p-0 rounded md:bg-transparent md:dark:text-secondary}`}
+                  aria-current={router.pathname === '/donate' ? "page" : undefined}
                 >
                   Be a pet hero
                 </a>
