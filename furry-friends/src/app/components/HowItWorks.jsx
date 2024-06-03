3ecc294ac2a0051c03509ba9b2cc618f010fe731
Mahama Hamida <mahamahamida@gmail.com>
2024-06-03 18:12:37 +0000

    feat: Update HowItWorks component to use lazy loading for images

--- furry-friends/src/app/components/HowItWorks.jsx
+++ furry-friends/src/app/components/HowItWorks.jsx
@@ -18,7 +18,7 @@ export default function HowItWorks() {
         <p>How it</p>
         <p className="text-accent">works?</p>
         </div>
-        <div className="flex flex-col gap-12 lg:flex-row justify-between">
+        <div className="flex flex-col gap-12 lg:flex-row items-center justify-between">
             <div className="flex flex-col justify-center gap-4 items-center w-full max-w-[334px]">
                 <div className="flex items-center justify-center rounded-full bg-palegreen w-[172px] h-[172px]">
                     <Image alt={'cat-phone'} width={200} height={200} className="h-[122px] w-auto" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588238/cat-phone_df1sxq.svg" />
