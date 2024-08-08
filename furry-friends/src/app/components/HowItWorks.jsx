796882f0188dc698c352d0e3e8ed883fe39264a8
Hamida Mahama <91345308+AngryDuchess@users.noreply.github.com>
2024-08-08 20:56:48 +0000

    Update HowItWorks.jsx

--- furry-friends/src/app/components/HowItWorks.jsx
+++ furry-friends/src/app/components/HowItWorks.jsx
@@ -8,7 +8,7 @@ export default function HowItWorks() {
         className="bg-secondary bg-blend-overlay py-12 px-4 flex flex-col gap-14 lg:px-16"
         style={{
           backgroundImage:
-            "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588251/second-bg_fkbbeg.png')",
+            "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588251/FurryFriends/second-bg_fkbbeg.png')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -21,7 +21,7 @@ export default function HowItWorks() {
         <div className="flex flex-col gap-12 lg:flex-row items-center justify-between">
             <div className="flex flex-col justify-center gap-4 items-center w-full max-w-[334px]">
                 <div className="flex items-center justify-center rounded-full bg-palegreen w-[172px] h-[172px]">
-                    <Image alt={'cat-phone'} width={200} height={200} className="h-[122px] w-auto" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588238/cat-phone_df1sxq.svg" />
+                    <Image alt={'cat-phone'} width={200} height={200} className="h-[122px] w-auto" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588238/FurryFriends/cat-phone_df1sxq.svg" />
                 </div>
                 <div className="text-center text-dark flex flex-col gap-3">
                     <p className="font-semibold text-accent">Create an account</p>
@@ -30,7 +30,7 @@ export default function HowItWorks() {
             </div>
             <div className="flex flex-col justify-center gap-4 items-center w-full max-w-[334px]">
                 <div className="flex items-center justify-center rounded-full bg-accentpalest w-[172px] h-[172px]">
-                    <Image alt={'cat-dog'} width={200} height={200} className="h-[122px] w-auto" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588239/cat-dog_kye9au.svg" />
+                    <Image alt={'cat-dog'} width={200} height={200} className="h-[122px] w-auto" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588239/FurryFriends/cat-dog_kye9au.svg" />
                 </div>
                 <div className="text-center text-dark flex flex-col gap-3">
                     <p className="font-semibold text-accent">Find a pet</p>
@@ -39,7 +39,7 @@ export default function HowItWorks() {
             </div>
             <div className="flex flex-col justify-center gap-4 items-center w-full max-w-[334px]">
                 <div className="flex items-center justify-center rounded-full bg-secondarypale w-[172px] h-[172px]">
-                    <Image width={200} height={200}  alt={'boss dog'} className="h-[122px]" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588238/boss-dog_fminzh.svg" />
+                    <Image width={200} height={200}  alt={'boss dog'} className="h-[122px]" src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716588238/FurryFriends/boss-dog_fminzh.svg" />
                 </div>
                 <div className="text-center text-dark flex flex-col gap-3">
                     <p className="font-semibold text-accent">Book a Call & Receive Your Pet</p>
