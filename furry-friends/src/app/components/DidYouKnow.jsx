2b80d578976c01360a0451f0a989dc3835ae7fdb
Hamida Mahama <91345308+AngryDuchess@users.noreply.github.com>
2024-08-08 20:59:39 +0000

    Update DidYouKnow.jsx

--- furry-friends/src/app/components/DidYouKnow.jsx
+++ furry-friends/src/app/components/DidYouKnow.jsx
@@ -8,7 +8,7 @@ export default function DidYouKnow() {
         className="bg-brown flex flex-col gap-4 justify-between md:flex-row text-dark px-4 md:px-16 bg-blend-overlay py-12"
         style={{
           backgroundImage:
-            "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716724548/question-marks_sauaie.png')",
+            "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716724548/FurryFriends/question-marks_sauaie.png')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -18,7 +18,7 @@ export default function DidYouKnow() {
           className="md:w-1/2 p-4 flex flex-col flex-grow gap-3 border border-secondarydeepest bg-secondarypalest"
           style={{
             backgroundImage:
-              "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716726033/noise_gttper.png')",
+              "url('https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716726033/FurryFriends/noise_gttper.png')",
               backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
@@ -30,7 +30,7 @@ export default function DidYouKnow() {
                 width={28}
                 height={28}
               className="w-[28px] h-[28px]"
-              src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716734291/light-bulb_1_i0nejh.svg"
+              src="https://res.cloudinary.com/dn5ks1ljf/image/upload/v1716734291/FurryFriends/light-bulb_1_i0nejh.svg"
             />
             <div className={`${merriweather.className} leading-7 font-bold`}>
               Did you <span className="text-light">know?</span>
@@ -47,7 +47,7 @@ export default function DidYouKnow() {
             </div>
         </div>
         <video className="flex-grow" autoPlay loop muted>
-          <source src="https://res.cloudinary.com/dn5ks1ljf/video/upload/v1716727483/cat-with-glasses_oehan5.mp4" type="video/mp4" />
+          <source src="https://res.cloudinary.com/dn5ks1ljf/video/upload/v1716727483/FurryFriends/cat-with-glasses_oehan5.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </section>
